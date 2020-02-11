32c897ee350b551766ca5bb92450f45864192a47
gal guy <yahavgal@gmail.com>
2020-02-11 14:37:34 -0800

    First Commit

---
+++ src/build/precache-manifest.255f7daa8c017d8cc4309d731c81b87e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15badb631e93a11960215e94c8ee7bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca0d24303b9c8f446be2",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "c31da800e162bd4eb5da",
+    "url": "/static/js/2.3ef046f8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.3ef046f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca0d24303b9c8f446be2",
+    "url": "/static/js/main.f5ad3d1b.chunk.js"
+  },
+  {
+    "revision": "30fa7e1a31a3024caf01",
+    "url": "/static/js/runtime-main.c9b77b4e.js"
+  }
+]);
